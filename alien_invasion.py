59367321f2b3a2e853ed8e85791114d118cb2ad7
tammi <tammiii@163.com>
2024-03-21 11:48:51 +0800

    setting

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,20 @@
 import sys
 import pygame
+from settings import Settings
 
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    # screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (230, 230, 230)  # 浅灰色
+    # bg_color = (230, 230, 230)  # 浅灰色
     # 红色(255, 0, 0) 绿色(0, 255, 0) 蓝色(0, 0, 255)
 
     while True:
-        screen.fill(bg_color)
+        # screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
