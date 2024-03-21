d33fe81efa9b4404207c00667f3ffae988fe8e7b
tammi <tammiii@163.com>
2024-03-21 14:05:55 +0800

    add ship in main

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -11,14 +12,16 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     # bg_color = (230, 230, 230)  # 浅灰色
     # 红色(255, 0, 0) 绿色(0, 255, 0) 蓝色(0, 0, 255)
+    ship = Ship(screen)  # 创建一艘飞船
 
     while True:
         # screen.fill(bg_color)
-        screen.fill(ai_settings.bg_color)
+        screen.fill(ai_settings.bg_color)  # 每次循环时都重绘屏幕
+        ship.blitme()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        pygame.display.flip()
+        pygame.display.flip() #让最近绘制的屏幕可见
 
 
 run_game()
